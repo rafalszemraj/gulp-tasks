4c493ddaf665ba8cc115089362f801dac17b5cce
Rafal Szemraj <rafal@szemraj.eu>
2015-09-26 17:05:51 +0200

    added .gitingore updated tasks

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 module.exports = {
 
-    report: require('./lib/report')
+    report          : require('./lib/report'),
+    clean           : require('./lib/clean'),
+    build           : require('./lib/build'),
+    serial          : require('./lib/multi').serial,
+    parallel        : require('./lib/multi').parallel,
+    uglify          : require('./lib/uglify'),
+    minify          : require('./lib/minify')
 
 }
