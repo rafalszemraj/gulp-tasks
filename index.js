ed9a0f8d500e1b5720aaafffe8898404a5bd09b8
Rafal Szemraj <rafal@szemraj.eu>
2015-10-06 20:08:04 +0200

    reverted: added glob for multiple browserify entries added test task (mocha)

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = {
     report          : require('./lib/report'),
     clean           : require('./lib/clean'),
     build           : require('./lib/build'),
+    test            : require('./lib/test'),
     serial          : require('./lib/multi').serial,
     parallel        : require('./lib/multi').parallel,
     uglify          : require('./lib/uglify'),
